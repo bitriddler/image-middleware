2e023d8b02f81a431a95c3ad71b08f94dfc3695f
Kareem Mohamed <kareem3d.a@gmail.com>
2016-05-04 11:45:18 +0200

    Return correct error

--- models/error.js
+++ models/error.js
@@ -1,7 +1,7 @@
 function error(code, msg, desc, type, errors) {
 	this.statusCode = code || require("../enums/errCode").NoCode;
 	this.error = {
-		message: msg || '',
+		message: typeof msg === 'object' ? msg.message : msg,
 		description: desc || '',
 		type: type || '',
 		errors: errors || null
